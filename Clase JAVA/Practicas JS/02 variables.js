9ce4dff524854ddd5de875500a36a13a8016fa6a
Frankquefut4 <frankquefut4@hotmail.com>
2025-07-30 23:21:08 -0700

    Avance 02 texto JS

---
+++ Clase JAVA/Practicas JS/02 variables.js
@@ -0,0 +1,36 @@
+
+
+
+
+/*a = 'valor'
+
+console.log(a);*/
+
+
+
+/*a = '15'
+palabra = a;
+console.log(palabra);*/
+
+/*a = 15;
+a = 20;
+console.log(a);*/
+
+/* var= a declarar variable, solo la declaras al principio,
+//  luego solo pones la variable sola.
+// se recomienta usar por el momento let
+let a = 15;
+a = 20;
+console.log(a);*/
+
+/* const: no cambia el valor
+const a = 15;
+a = 20;
+
+console.log(a);*/
+
+let a = 15, b = 30;
+a = 20;
+console.log(a, b);
+
+
---
+++ Clase JAVA/Practicas JS/03 texto.js
@@ -0,0 +1,21 @@
+/*let libro = 'Harry Potter';
+let segundoLibro = "Pragmatic Programmer";
+
+console.log(libro, segundoLibro);*/
+
+
+
+let libro = 'Harry\n\
+Potter\'s\n\Castle';
+let segundoLibro = "Pragmatic 'Programmer'";
+
+console.log(libro, segundoLibro);
+
+
+/*let libro = 'Harry Potter';             
+console.log(libro);*/
+
+/* como usar las comillas "" '': sirven igual, solo que se usan para separar el lexico
+let libro = "Harry potter's Diary";     otro:'Harry "potter" Diary' el mejor:'Harry \ 'potter\ ' Diary' 
+console.log(libro);*/
+
