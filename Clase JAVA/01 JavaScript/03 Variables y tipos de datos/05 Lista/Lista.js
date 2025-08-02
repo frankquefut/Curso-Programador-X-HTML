8b1b9cdffaf372dac1a4c82a860a18dad309ed24
frankquefut4 <frankquefut4@hotmail.com>
2025-08-01 17:14:20 -0700

    java list

--- Clase JAVA/01 JavaScript/03 Variables y tipos de datos/05 Lista/Lista.js
+++ Clase JAVA/01 JavaScript/03 Variables y tipos de datos/05 Lista/Lista.js
@@ -1,7 +1,7 @@
 let listaVacia = [];
 let listaDeNumeros = [1,2,3,4,5];
 let listaDeTextos = ['Lunes', 'Martes', 'Miércoles'];
-let listaRandom = [1, 'Lunes', true, {}, [false]];
+let listaRandom = [1, 'Lunes', true, {}, [false, true]];
 
 // console.log(listaDeTextos);
 // console.log(listaRandom);
@@ -10,4 +10,4 @@ let listaRandom = [1, 'Lunes', true, {}, [false]];
 // console.log(listaRandom[2]);
 // console.log(listaRandom[3]);
 // console.log(listaRandom[4]);
-console.log(listaRandom[4][0]);
+console.log(listaRandom[4][1]);
