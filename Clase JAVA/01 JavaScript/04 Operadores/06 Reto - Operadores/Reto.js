941618f75bd57313187b643791d65c4b2098f1e2
Frankquefut4 <frankquefut4@hotmail.com>
2025-08-04 22:56:45 -0700

    Terminamos Reto  Operadores

---
+++ Clase JAVA/01 JavaScript/04 Operadores/06 Reto - Operadores/Reto.js
@@ -0,0 +1,30 @@
+// 10 y 5
+let a = 10;
+let b = 5;
+console.log('¿El numero 10 es mayor que el numero 5? ' + (a > b));
+console.log('¿El numero 10 es menor que el numero 5? ' + (a < b));
+
+// 1 y 2
+let c = 1;
+let d = 2;
+console.log('¿El numero 1 es igual al numero 2? ' + (c === d));
+
+// Textos
+let texto1 = {nombre: 'Francisco'};
+let texto2 = {snombre: 'Arturo'};
+console.log('¿El texto 1 es igual al texto 2? ' + (texto1.nombre === texto2.snombre));
+
+//objetos
+console.log('¿Los objetos son iguales? ' + (texto1.nombre === texto2.snombre))
+
+// AND Y OR 
+console.log('AND logico: ' + (a < b && a > d));
+console.log('OR logico: ' + (a < b || a > d));
+
+// 10 y 3
+let e = 3;
+console.log('La suma de 10 + 3: ' + (a + e));
+console.log('La resta de 10 - 3: ' + (a - e));
+console.log('La multiplicacion  de 10 * 3: ' + (a * e));
+console.log('La division de 10 / 3: ' + (a / e));
+console.log('10 modulo de 3: ' + (a % e));
