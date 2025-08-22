2c8c688719352fc15ad130fca091c4dd438f8945
frankquefut4 <frankquefut4@hotmail.com>
2025-08-22 15:44:24 -0700

    Ignorar carpeta 09 Módulos y limpiar archivos eliminados

--- Clase JAVA/01 JavaScript/09 Módulos/03 Reto - Módulos/validacionCorreo.js
+++
@@ -1,5 +0,0 @@
-// Función para validar correo electrónico y exportarla
-export function validarCorreo(correo) {
-    const patronCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return patronCorreo.test(correo);
-}
